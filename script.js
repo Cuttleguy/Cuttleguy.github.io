c664c645a19531197da5419b3b41b4423794fb4e
Cuttleguy <maxg@primorisacademy.org>
2023-09-01 15:24:12 -0400

    Finished Text

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 var text= document.getElementById("text");
+var strtext="Happy Birthday!"
+var colorIndex=0;
+var colors=["#ff0000","#ff6600","#ffff00","#00ff00","#0000ff","#4b0082","#ff00ff"]
+function colorChange(){
+text.innerHTML="";
+for (let index = 0; index < strtext.length; index++) {
+    
+    const element = strtext[index];
+    text.innerHTML+="<div id="+"char"+index+">"+element+"</div>";
+    divis=document.getElementById("char"+index);
+    divis.style.color=colors[(colorIndex+index)%7]
+
+}
+colorIndex+=1;
+
+}
+setInterval(colorChange,750);
