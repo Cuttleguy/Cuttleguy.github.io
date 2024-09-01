bcb49c5fe6297917fdb7a3be48f8b5677e8cd817
Cuttleguy <123271904+Cuttleguy@users.noreply.github.com>
2024-09-01 12:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var text= document.getElementById("text");
 var strtext="I hope you have the BEST BIRTHDAY EVER DADDY!"
 var colorIndex=0;
-var colors=["#ff0000","#ff6600","#ffff00","#00ff00","#0000ff","#4b0082","#ff00ff"]
+var colors=["#ff0000","#ff6600","#ffff00","#00ff00","#0000ff","#4b0082","#880088"]
 function colorChange(){
 text.innerHTML="";
 for (let index = 0; index < strtext.length; index++) {
